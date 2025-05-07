6c8b5e16b322f23eac69fc49008e68954a14f4f0
Onja <harisoaonja06@gmail.com>
2025-05-07 15:52:55 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("savePlan").addEventListener("click", function() {
+    alert("Plan sauvegardé !");
+});
+
+// Système d’abonnement (simulation)
+document.querySelector(".subscribe").addEventListener("click", function() {
+    alert("Système d’abonnement en cours de développement !");
+});
